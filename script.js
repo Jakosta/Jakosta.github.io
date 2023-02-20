d613867caf168e00faddbfec587dbf0fadf1ceda
Mariano Acosta <33203809+Jakosta@users.noreply.github.com>
2023-02-20 15:40:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleSkills() {
+	var skillsSection = document.getElementsByTagName("section")[2];
+	if (skillsSection.style.display === "none") {
+		skillsSection.style.display = "block";
+	} else {
+		skillsSection.style.display = "none"
+    }}
